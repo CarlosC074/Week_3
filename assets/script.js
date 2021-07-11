bdf73e410095e04c790aef02411019b3ac9a6cb7
CarlosC074 <carlos.castano74@gmail.com>
2021-07-11 00:57:45 -0700

    Added basic HTML with a button to excecute the code once it's done. Added a text document to note my plans with javascript. CSS added to center the content. All characters and arrays added to javascript

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+// Any sort of quotation has been excluded from the array since the system keeps trying to interpret a string from it.
+var allChar = [q,w,e,r,t,y,u,i,o,p,a,s,d,f,g,h,j,k,l,z,x,c,v,b,n,m,Q,W,E,R,T,Y,U,I,O,P,A,S,D,F,G,H,J,K,L,Z,X,C,V,B,N,M,1,2,3,4,5,6,7,8,9,0,
+    !,#,$,%,&,,(,),*,+,,,-,.,/,:,;,<,=,>,?,@,[,\,],^,_,,{,|,},~];
+
+var lowerCase = [q,w,e,r,t,y,u,i,o,p,a,s,d,f,g,h,j,k,l,z,x,c,v,b,n,m];
+
+var upperCase = [Q,W,E,R,T,Y,U,I,O,P,A,S,D,F,G,H,J,K,L,Z,X,C,V,B,N,M];
+
+var numerals = [1,2,3,4,5,6,7,8,9,0];
+
+var specialChar = [!,#,$,%,&,,(,),*,+,,,-,.,/,:,;,<,=,>,?,@,[,\,],^,_,,{,|,},~];
+
+function generate() {
+    alert("Sample text");
+}
+
